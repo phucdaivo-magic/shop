5185549fcb194674c37ce0916d6535830a034750
phucdaivo <daihandaiphuc@gmail.com>
2022-03-22 00:47:14 +0700

    ad

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -70,7 +70,7 @@ class ProductController extends AdminController
                     ],
                     [
                         'view' => function ($product) {
-                            return '<div style="max-width: 80px">'.$product->trademark->name ?? ''.'<div>';
+                            return '<div style="max-width: 80px">'.($product->trademark->name ?? '').'<div>';
                         },
                         'search' => [
                             'attrs' => [
