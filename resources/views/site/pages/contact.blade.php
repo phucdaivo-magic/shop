48f1f213f94a75bf3284219dacad33dbe42950dc
phucdaivo <daihandaiphuc@gmail.com>
2022-03-24 11:17:06 +0700

    Add-Site-Contact

--- resources/views/site/pages/contact.blade.php
+++ resources/views/site/pages/contact.blade.php
@@ -7,8 +7,38 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="alert alert-warning mt-4" role="alert">
-                Trang web đang được xây dựng.
+            <div class="row pt-5">
+                <div class="col-md-6">
+                    <h3>Shop.vn – Care for your Mac</h3>
+                    <p>Giới thiệu</p>
+                    <div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.212841519796!2d106.71968101458012!3d10.795004161802892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529a1602a74c1%3A0x42fd34e413987eae!2sLandmark%205%20Vinhomes%20Central%20Park!5e0!3m2!1svi!2s!4v1648094341897!5m2!1svi!2s" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <p>Bạn có thể điền thông tin liên hệ vào form dưới đây Shop sẽ liên hệ ngay với bạn khi nhận được thông tin.</p>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Họ và tên</label>
+                        <input type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Họ và tên">
+                        <small hidden id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Địa chỉ email</label>
+                        <input type="email" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Địa chỉ email">
+                        <small hidden id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Số điện thoại</label>
+                        <input type="email" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Số điện thoại">
+                        <small hidden id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nội dung liên hệ</label>
+                        <textarea class="form-control rounded-0" name="" id="" cols="30" rows="6" placeholder="Nội dung liên hệ"></textarea>
+                        <small hidden id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <button class="btn btn-danger mb-3 pl-4 pr-4 rounded-0">Gửi</button>
+                </div>
             </div>
         </div>
     </div>
