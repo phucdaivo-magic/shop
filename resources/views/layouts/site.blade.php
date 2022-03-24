71a5daaa8d50ce284e739b724f125f31990c2c7f
phucdaivo <daihandaiphuc@gmail.com>
2022-03-24 23:07:39 +0700

    Fix site

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -145,8 +145,7 @@ $categoryList = App\Models\Category::where('home', true)
         </div>
     </div>
     <script>
-        window.BASE_URL = '{{ url('
-        ') }}';
+        window.BASE_URL = '{{ url('') }}';
     </script>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
