c75abbfb2515b98597feecdea0b9a99336612d44
phucdaivo <daihandaiphuc@gmail.com>
2022-03-25 22:29:35 +0700

    Update code

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
@@ -11,13 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => 'admin',
-                'email' => 'admin@admin.com',
-                'password' => '$2y$10$9TXEn/tnO0wpMpFf7nw7ae99PHKOxla/ialaNb7UNYGKQhpMM6FY2',
-            ]
-        ]);
+        // DB::table('users')->insert([
+        //     [
+        //         'name' => 'admin',
+        //         'email' => 'admin@admin.com',
+        //         'password' => '$2y$10$9TXEn/tnO0wpMpFf7nw7ae99PHKOxla/ialaNb7UNYGKQhpMM6FY2',
+        //     ]
+        // ]);
+
+        $controller = new ProductController();
+
+        $products = Product::get();
+        foreach ($products  as $key => $product) {
+            $controller->clone($product);
+        }
+
+
+
+        // $products = Product::where('sort', '>', 45)->get();
+        // foreach ($products  as $key => $product) {
+        //     $product->delete();
+        // }
 
     }
 }
