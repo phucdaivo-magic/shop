ce9bb9403d6f8b17de49b82f93544444328aa515
phucdaivo <daihandaiphuc@gmail.com>
2022-03-25 21:39:42 +0700

    Update iamge in table

--- resources/views/admin/components/image.blade.php
+++ resources/views/admin/components/image.blade.php
@@ -1,3 +1,3 @@
-<div data-fancybox="gallery-a" data-src="{{asset($item[$option['key']])}}">
+<div style="max-width: 100px;" data-fancybox="gallery-a" data-src="{{asset($item[$option['key']])}}">
     <img style="width: 100%" src="{{asset($item[$option['key']])}}" alt="">
 </div>
